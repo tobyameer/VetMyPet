f3ffc60945f6f101380036cd42756a8ff8565d33
Mohsen Ameer <mohsenameer15@gmail.com>
2024-12-13 02:59:35 +0200

    adding backend for signup and login

--- Server/index.js
+++ Server/index.js
@@ -4,24 +4,46 @@ const cors = require("cors");
 const UserModel = require("./models/User");
 const dotenv = require("dotenv");
 const bcrypt = require("bcrypt");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: process.env.FRONT_URL,
+    credentials: true,
+  })
+);
 
 // Connecting to MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/ClientData")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Failed to connect to MongoDB", err));
+
+// Sessions
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+  })
+);
 
 // Connecting to Port
 app.listen(process.env.PORT, () => {
   console.log(`Server is Running on Port ${process.env.PORT}`);
 });
 
+// Sign Up
 app.post("/signup", async (req, res) => {
   try {
     const { name, lastName, email, password } = req.body;
@@ -32,10 +54,51 @@ app.post("/signup", async (req, res) => {
       return res.status(400).json({ error: "email already taken" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new UserModel({ name, email, password: hashedPassword });
+    const newUser = new UserModel({
+      name,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
     const savedUser = await newUser.save();
     express.status(201).json(savedUser);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+// Log In
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (passwordMatch) {
+        req.session.user = {
+          id: user._id,
+          name: user.name,
+          lastName: user.lastName,
+          email: user.email,
+        };
+        res.json("Success");
+        console.log(email);
+        console.log(user.name);
+      } else {
+        res.status(401).json("Password dose'nt match");
+      }
+    } else {
+      res.status(404).json("No Records found");
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/user", (req, res) => {
+  if (req.session.user) {
+    res.json({ user: req.session.user });
+  } else {
+    res.status(401).json("Not authenticated");
   }
 });
